4b083e468343e517abc6cae35e94d8fcfabb53a4
anna.marczynska <anna.m.marczynska@gmail.com>
2020-03-22 19:31:03 +0100

    Browse button update

--- public/controllers/music.controller.js
+++ public/controllers/music.controller.js
@@ -20,16 +20,27 @@
         .controller("MusicCtrl", MusicCtrl);
 
     function MusicCtrl($scope, MusicService) {
+        $scope.showMusicBrowser = false;
+        $scope.showMusicSheet = true;
+        $scope.result = "";
+        $scope.songsArray = [];
+        $scope.chosenSong;
+
+        $scope.toggleMusicBrowser = function () {
+            $scope.showMusicBrowser = !$scope.showMusicBrowser;
+            $scope.showMusicSheet = !$scope.showMusicSheet;
+            console.log($scope.chosenSong)
+        }
         $scope.browseMusic = function () {
             MusicService
                 .browseMusic()
                 .then(
-                function (response) {
-                    console.log(response)
-                },
-                function (err) {
-                    $scope.error = err;
-                }
+                    function (response) {
+                        console.log(response)
+                    },
+                    function (err) {
+                        $scope.error = err;
+                    }
                 )
         }
     }
