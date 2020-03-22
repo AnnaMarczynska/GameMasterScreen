19c9807006006d7288b07807b44dcc1dcb729632
anna.marczynska <anna.m.marczynska@gmail.com>
2020-03-22 16:43:52 +0100

    Stop button add

--- public/javascripts/music.js
+++ public/javascripts/music.js
@@ -3,10 +3,12 @@ window.onload = function(){
     var myAudio = document.getElementById('my-audio');
     var play = document.getElementById('play');
     var pause = document.getElementById('pause');
+    var stop = document.getElementById('stop');
   
     // associate functions with the 'onclick' events
     play.onclick = playAudio;
     pause.onclick = pauseAudio;
+    stop.onclick = stopAudio;
   
     function playAudio() {
       myAudio.play();
@@ -14,6 +16,11 @@ window.onload = function(){
   
     function pauseAudio() {
       myAudio.pause();
+    }
+
+    function stopAudio() {
+        myAudio.pause();
+        myAudio.currentTime = 0;
     }
   
   }
